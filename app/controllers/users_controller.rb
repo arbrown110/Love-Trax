75aaf3c0a50d858909cca8a0e69081c47d7e59ae
arbrown110 <arbrown110@gmail.com>
2020-07-07 20:48:31 -0400

    show creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
     get '/users/sign_in' do
         erb :'/users/sign_in'
     end
+
+    post '/users/sign_in' do
+        @user = User.create(username: params[:username], password: params[:password])
+        redirect "/users/#{@user.id}"
+    end
+
+    get '/users/:id' do   #show page render data of one instance
+        erb :'/users/show'
+        @user = User.find(params[:id])
+    end    
 end
