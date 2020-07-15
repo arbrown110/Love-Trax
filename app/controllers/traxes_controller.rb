0f8a8c515443047052694d900000c36bf6a39b9f
arbrown110 <arbrown110@gmail.com>
2020-07-15 00:47:41 -0400

    CRUD update

--- app/controllers/traxes_controller.rb
+++ app/controllers/traxes_controller.rb
@@ -1,26 +1,54 @@
 class TraxesController < ApplicationController
 
+ # route to delete 
+ delete '/traxes/:id' do
+  binding.pry
+  gone = set_trax_entry
+  if session[:user_id] != gone.user_id
+  not_authorized_to_edit(@trax)
+  else gone
+    gone.destroy
+    #flash[:message] = "The entry is delete."
+    redirect "/traxes/experience"
+  end  
+end
+  
+
   get '/traxes' do
     redirect_if_not_signed_in
     @user = current_user
     @trax = @user.traxes
-    #erb :'users/show'
-    #erb :'/traxes/show' #shows empty experience
-    erb :'traxes/experience'
+    erb :'traxes/index'
   end
 
+
+
+  patch '/traxes/:id' do
+    redirect_if_not_signed_in
+    
+    yo = current_user
+    set_trax_entry
+
+    not_authorized_to_edit(@trax)
+     if params[:name] !="" && params[:date] !="" && params[:score] !="" && params[:location] !="" && params[:number] !="" && params[:interest] 
+     @trax.update(name: params[:name], date: params[:date], score: params[:score], location: params[:location], number: params[:number], interest: params[:interest]) 
+      redirect "/traxes/#{@trax.id}"
+     else
+       redirect "/traxes/#{@trax.id}/edit"
+     end 
+  end
   
 
 # Makes a new friend
   get '/traxes/new' do
     redirect_if_not_signed_in
-    #binding.pry
     @user = current_user
     @trax = Trax.all
     
     erb :'/traxes/new'
   end
 
+  
   post '/traxes/new' do
     redirect_if_not_signed_in
     @trax=Trax.create(name: params[:name], date: params[:date], 
@@ -33,6 +61,14 @@ class TraxesController < ApplicationController
     redirect "/traxes/#{@trax.id}"
   end
 
+  get '/traxes/:id/edit' do
+    redirect_if_not_signed_in
+    set_trax_entry
+    erb :'/traxes/edit'
+  end
+
+
+
   # show route for one trax experience
   get '/traxes/:id' do
     redirect_if_not_signed_in
@@ -41,45 +77,48 @@ class TraxesController < ApplicationController
        #flash[:errors] = "Please select a conference from the list on the conferences page."
        redirect "/sign_in"   
      end
-      erb :'/traxes/experience'  #What does your dating life look if? coming from /TRAXES 
+      erb :'/traxes/show'  #What does your dating life look if? coming from /TRAXES 
     end
   end
 
-  get '/traxes/:id/edit' do
-    redirect_if_not_signed_in
-    binding.pry
-    set_trax_entry
-    erb :'/traxes/edit'
-  end
+  # get '/traxes/:id/edit' do
+  #   redirect_if_not_signed_in
+  #   binding.pry
+  #   set_trax_entry
+  #   erb :'/traxes/edit'
+  # end
 
-  patch '/traxes/:id' do
-    redirect_if_not_signed_in
-    binding.pry
-    yo = current_user
-    set_trax_entry
+  # patch '/traxes/:id' do
+  #   redirect_if_not_signed_in
+  #    binding.pry
+  #   yo = current_user
+  #   set_trax_entry
 
-   # not_authorized_to_edit(trax)
-    #  if params[:name] !="" && params[:date] !="" && params[:score] !="" && params[:location] !="" && params[:number] !="" && params[:interest] 
-    #    @trax.update(name: params[:name], date: params[:date], score: params[:score], location: params[:location], number: params[:number], interest: params[:interest]) 
-    #    redirect "/traxes/#{yo.id}"
-    #  else
-    #    redirect "/traxes/#{@trax.id}/edit"
-    #  end
-      #redirect "/traxes/show" 
-  end
+  #   not_authorized_to_edit(trax)
+  #    if params[:name] !="" && params[:date] !="" && params[:score] !="" && params[:location] !="" && params[:number] !="" && params[:interest] 
+  #    @trax.update(name: params[:name], date: params[:date], score: params[:score], location: params[:location], number: params[:number], interest: params[:interest]) 
+  #     # redirect "/traxes/#{yo.id}"
+  #     redirect "/traxes/#{@trax.id}"
+  #    else
+  #      redirect "/traxes/#{@trax.id}/edit"
+  #    end
+  #     # redirect "/traxes/#{@trax.id}/show"
+   
+  # end
 
 
 # route to delete 
-  delete '/traxes/:id' do
-    gone = set_trax_entry
-    if session[:user_id] != gone.user_id
-    not_authorized_to_edit(@trax)
-    else gone
-      gone.destroy
-      #flash[:message] = "The entry is delete."
-      redirect "/traxes/experience"
-    end  
-  end
+  # delete '/traxes/:id' do
+  #   binding.pry
+  #   gone = set_trax_entry
+  #   if session[:user_id] != gone.user_id
+  #   not_authorized_to_edit(@trax)
+  #   else gone
+  #     gone.destroy
+  #     #flash[:message] = "The entry is delete."
+  #     redirect "/traxes/experience"
+  #   end  
+  # end
 
   private
 
